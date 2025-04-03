148e61718e12d86c8e4aa89264e969562d441f7a
GijsKuijpers1 <gb.kuijpers@student.han.nl>
2025-04-03 20:19:44 +0200

    Planetentext glitcht help

--- src/main/java/org/example/entities/text/PlanetenText.java
+++ src/main/java/org/example/entities/text/PlanetenText.java
@@ -6,33 +6,28 @@ import com.github.hanyaeger.api.entities.impl.CustomFont;
 import com.github.hanyaeger.api.entities.impl.DynamicTextEntity;
 import javafx.scene.paint.Color;
 import org.example.Spawners.ObjectenSpawner;
-import org.example.entities.sliceable.planeten.Planeet;
 import org.example.scenes.GameScene;
 
 public class PlanetenText extends DynamicTextEntity implements UpdateExposer {
     private int score = 0;
     private GameScene gameScene;
-
-    public PlanetenText(Coordinate2D initialLocation, GameScene gameScene){
+    ObjectenSpawner objectenSpawner;
+    public PlanetenText(Coordinate2D initialLocation, GameScene gameScene, ObjectenSpawner objectenSpawner){
         super(initialLocation);
         setFont(new CustomFont("fonts/Minecraft.ttf", 10));
         setFill(Color.WHITE);
-        setText("0");
+        setText("plane: 0");
         this.gameScene = gameScene;
-
-
+        this.objectenSpawner = objectenSpawner;
         setViewOrder(10);
 
     }
 
     @Override
     public void explicitUpdate(long l) {
-
-        Planeet planeet = gameScene.getPlaneet();
-        if(planeet != null){
-            setText("" + planeet.getGeslicedePlaneten());
+        if (objectenSpawner != null) {
+//            Planeet planeet = objectenSpawner.getPlaneet();
+//            setText("planeten: " + (planeet.getGeslicedePlaneten()));
         }
-
-
     }
 }
